1129fbcd252d776630ef556ce47f74f7c5469f25
Artem Volkov <mesmer_93@mail.ru>
2020-02-28 20:54:16 +0300

    Auth

--- back/app.js
+++ back/app.js
@@ -1,33 +1,35 @@
+const useMiddleware = require('./middleware');
 const createError = require('http-errors');
+
+useMiddleware(app);
 const express = require('express');
-const useMiddleware = require("./middleware");
 
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-useMiddleware(app);
 
 // Подключаем mongoose.
 const mongoose = require('mongoose');
+
 mongoose.connect(
-  "mongodb://localhost:27017/TestApp",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  'mongodb://localhost:27017/CompanyListApp',
+  { useNewUrlParser: true, useUnifiedTopology: true },
 );
 
 app.use('/api/users/', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-console.error(err);
+  console.error(err);
   // render the error page
   res.status(err.status || 500);
   res.send('error');
